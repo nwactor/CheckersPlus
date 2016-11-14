a78d41b436e4aed43812d33995e999527147c246
igourde <iriegourde@gmail.com>
2016-11-13 22:07:28 -0500

    Options menu activity
    Can toggle between main and options menu

--- src/src/main/java/com/example/nick/checkers/OptionsInterface.java
+++ src/src/main/java/com/example/nick/checkers/OptionsInterface.java
@@ -1,7 +1,11 @@
 package com.example.nick.checkers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class OptionsInterface extends AppCompatActivity {
 
@@ -9,7 +13,23 @@ public class OptionsInterface extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_interface);
+        Intent intent = getIntent();
+        String message = ("DeezNuts");
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+    }
+
+    public void retrurnToMain(View v){
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
+
+    }
 
 
     }
-}
+
+
+
+
